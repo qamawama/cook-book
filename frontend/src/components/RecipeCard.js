00557717c406772c32e86b17091155bb7080946f
qamawama <noorqamarina33@gmail.com>
2026-02-02 15:16:15 +0800

    Add recipe card component

---
+++ frontend/src/components/RecipeCard.js
@@ -0,0 +1,20 @@
+import {Link} from "react-router-dom";
+import './RecipeCard.css';
+
+function RecipeCard({recipe}) {
+    return (
+        <div className="recipe-card">
+            <div className="recipe-card-header">
+                <h3>{recipe.title}</h3>
+                {recipe.category && <span className="recipe-category">{recipe.category}</span>}
+            </div>
+
+            <p className="recipe-description">{recipe.description}</p>
+
+            <Link to={`/recipe/${recipe.id}`} className="btn-view">
+                View Recipe
+            </Link>
+        </div>
+    );
+}
+export default RecipeCard;
