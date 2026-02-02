00f55af346f9caaddee43b5be8c759d2b8b2ac83
qamawama <noorqamarina33@gmail.com>
2026-02-02 15:19:41 +0800

    Improve UI styling

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import {Link, useNavigate} from "react-router-dom";
 import {login} from "../services/api";
+import './Auth.css';
 
 function Login() {
     const [formData, setFormData] = useState({
